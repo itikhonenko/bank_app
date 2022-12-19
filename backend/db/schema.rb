168f951642e5f25160e80228f92c1c273b34f2dc
IT086469 <IT086469@cerner.net>
2022-12-19 09:53:11 +0200

    [Fix] update docker-compose

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,43 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_221_215_192_412) do
+ActiveRecord::Schema.define(version: 2022_12_15_192412) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'accounts', force: :cascade do |t|
-    t.decimal 'balance', precision: 8, scale: 2, default: '0.0'
-    t.integer 'account_type', default: 0
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "accounts", force: :cascade do |t|
+    t.decimal "balance", precision: 8, scale: 2, default: "0.0"
+    t.integer "account_type", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'transactions', force: :cascade do |t|
-    t.bigint 'from_account_id'
-    t.bigint 'to_account_id'
-    t.decimal 'amount', precision: 8, scale: 2
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['from_account_id'], name: 'index_transactions_on_from_account_id'
-    t.index ['to_account_id'], name: 'index_transactions_on_to_account_id'
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "from_account_id"
+    t.bigint "to_account_id"
+    t.decimal "amount", precision: 8, scale: 2
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["from_account_id"], name: "index_transactions_on_from_account_id"
+    t.index ["to_account_id"], name: "index_transactions_on_to_account_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users_accounts', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'account_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['account_id'], name: 'index_users_accounts_on_account_id'
-    t.index ['user_id'], name: 'index_users_accounts_on_user_id'
+  create_table "users_accounts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_users_accounts_on_account_id"
+    t.index ["user_id"], name: "index_users_accounts_on_user_id"
   end
 
-  add_foreign_key 'users_accounts', 'accounts'
-  add_foreign_key 'users_accounts', 'users'
+  add_foreign_key "users_accounts", "accounts"
+  add_foreign_key "users_accounts", "users"
 end
